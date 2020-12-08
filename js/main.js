d61aab0034932f487fe02f13009d88fe99aece43
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-08 22:46:07 +0200

    random car update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,48 @@
 const car1 = document.querySelector('.car-1');
 const car2 = document.querySelector('.car-2');
 
+const models = ['astra', 'astra2', 'avencis', 'forester', 'kia', 'saab', 'outback', 'verso']
+
+console.log(models);
 const windowWidth = innerWidth;
 const carWidth = 150;
 const finishwidth = 40;
 const finishRightSpace = 10;
+const carStep = 50;
 
 const finishPosition = windowWidth - carWidth - finishwidth - finishRightSpace;
 
+const car1Model = Math.floor(Math.random() * models.length);
+const car2Model = Math.floor(Math.random() * models.length);
+
+car1.dataset.model = models[car1Model];
+car2.dataset.model = models[car2Model];
+
 let car1Position = 0;
 let car2Position = 0;
+
+car1.style.left = car1Position;
+car2.style.left = car2Position;
 
 // aaddEventListener('keypress'); /* registruoja ir paspaudima ir atspaudimapaspaudimas */
 // addEventListener('keydown');  /* registruoja tik kai ispaudi  */
 addEventListener('keyup',(event) => {
     const key = event.key;
     if(key === 'w') {
-        car1Position += 10;
+        car1Position += carStep;
         car1.style.left = car1Position + 'px';
         if (car1Position > finishPosition) {
             alert('laimejo pirmas automobilis!')
+            location.reload();
         }
     }
 
     if(key === 'p') {
-        car2Position += 10;
+        car2Position += carStep;
         car2.style.left = car2Position + 'px';
         if (car2Position > finishPosition) {
             alert ('laimejo antras automobilis!')
+            location.reload();
         }
         
     }
