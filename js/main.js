c0d2750df17d315f52ee3402f68e7afa87fc6d47
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-28 22:09:31 +0200

    main.js updates

--- js/main.js
+++ js/main.js
@@ -9,40 +9,42 @@ const winP = winBlock.querySelector('p');
 const playAgain = winBlock.querySelector('button');
 let victory = false;
 
-
 // const models = ['astra', 'astra2', 'avencis', 'forester', 'kia', 'saab', 'outback', 'verso']
-
-
-const windowWidth = innerWidth;
-const carWidth = 150;
-const finishwidth = 40;
-const finishRightSpace = 10;
-const carStep = 30;
-
-const finishPosition = windowWidth - carWidth - finishwidth - finishRightSpace;
 let car1Position = 0;
 let car2Position = 0;
 
 const car1Model = getModel(models);   /*Math.floor(Math.random() * models.length);*/
 const car2Model = getModel(models);  /*Math.floor(Math.random() * models.length);*/
 
-car1.dataset.model = models[car1Model];
-car2.dataset.model = models[car2Model];
+car1.dataset.model = car1Model;
+car2.dataset.model = car2Model;
+
+const windowWidth = innerWidth;
+const carWidth = 150;
+const finishwidth = 40;
+const finishRightGap = 10;
+// const carStep = 30;
+const distanceToFinish = windowWidth - carWidth - finishwidth - finishRightGap;
+
+addEventListener('keyup', (event) => {
+    if (victory) {
+        return;
+    }
 
 
+// car1.style.left = car1Position;
+// car2.style.left = car2Position;
 
-car1.style.left = car1Position;
-car2.style.left = car2Position;
 
-// aaddEventListener('keypress'); /* registruoja ir paspaudima ir atspaudimapaspaudimas */
+// addEventListener('keypress'); /* registruoja ir paspaudima ir atspaudimapaspaudimas */
 // addEventListener('keydown');  /* registruoja tik kai ispaudi  */
 addEventListener('keyup',(event) => {
     const key = event.key;
     if(key === 'w') {
-        car1Position += carStep;
+        car1Position += 30;
         car1.style.left = car1Position + 'px';
-        if (car1Position > finishPosition) {
-            alert('laimejo pirmas automobilis!')
+        if (car1Position > distanceToFinish) {
+            victory = true;
             location.reload();
         }
     }
@@ -58,6 +60,4 @@ addEventListener('keyup',(event) => {
     }
 
 
-});  
-
-
+}) 
